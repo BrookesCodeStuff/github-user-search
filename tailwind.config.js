7c62c8dfc3690a6fdcfa31b1fdd74476c246c67a
Brooke <brookepaglia@gmail.com>
2022-06-11 22:31:03 -0400

    reorg files and add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./assets/js/*.js"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
